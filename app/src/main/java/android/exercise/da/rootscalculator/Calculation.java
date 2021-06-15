df0d3fa3b49adeb260e92c1f0e98b2cd5d018ba7
Daniel Abayev <danieldaniel151@gmail.com>
2021-06-15 15:33:24 +0300

    added calculations holder

---
+++ app/src/main/java/android/exercise/da/rootscalculator/Calculation.java
@@ -0,0 +1,47 @@
+package android.exercise.da.rootscalculator;
+
+import java.util.UUID;
+
+public class Calculation implements Comparable<Calculation> {
+    long number;
+    long root1, root2;
+    String status;
+    String calcId;
+
+    public Calculation(long number) {
+        this.number = number;
+        this.status = "in-progress";
+        this.root1 = -1;
+        this.root2 = -1;
+        this.calcId = UUID.randomUUID().toString();
+    }
+
+    public void setRoots(long root1, long root2) {
+        this.root1 = root1;
+        this.root2 = root2;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getNumber() {
+        return this.number;
+    }
+
+    public String getCalcId() {
+        return calcId;
+    }
+
+    @Override
+    public int compareTo(Calculation o) {
+        if (this.status.equals("in-progress") && o.status.equals("in-progress") ||
+                this.status.equals("done") && o.status.equals("done")) {
+            return Double.compare(this.number, o.number);
+        } else if (this.status.equals("in-progress") && o.status.equals("done")) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ app/src/main/java/android/exercise/da/rootscalculator/CalculationsHolder.java
@@ -0,0 +1,73 @@
+package android.exercise.da.rootscalculator;
+
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+
+public class CalculationsHolder {
+    private ArrayList<Calculation> calculationList = new ArrayList<>();
+    private final SharedPreferences sp;
+    private Gson gson;
+    private MutableLiveData<ArrayList<Calculation>> calculationsLiveDataMutable = new MutableLiveData<>();
+
+    CalculationsHolder(SharedPreferences sp) {
+        this.sp = sp;
+        this.gson = new Gson();
+        initializeFromSp();
+        this.calculationsLiveDataMutable.setValue(new ArrayList<>(calculationList));
+
+    }
+
+    private void initializeFromSp() {
+        Set<String> keys = this.sp.getAll().keySet();
+        for (String key : keys) {
+            String todoItem = sp.getString(key, null);
+            Calculation calc = gson.fromJson(todoItem, Calculation.class);
+            calculationList.add(calc);
+        }
+        Collections.sort(this.calculationList);
+    }
+
+    public ArrayList<Calculation> getCurrentItems() {
+        return new ArrayList<>(this.calculationList);
+    }
+
+    public void addNewCalculation(long number) {
+        Calculation calc = new Calculation(number);
+        this.calculationList.add(calc);
+        Collections.sort(this.calculationList);
+        calculationsLiveDataMutable.setValue(new ArrayList<>(this.calculationList));
+        updateSpContent(calc);
+    }
+
+    public LiveData<ArrayList<Calculation>> getLiveData() {
+        return this.calculationsLiveDataMutable;
+    }
+
+    public void markCalcDone(Calculation calc) {
+        for (int i = 0; i < this.calculationList.size(); i++) {
+            if (this.calculationList.get(i).getCalcId().equals(calc.getCalcId())) {
+                this.calculationList.get(i).setStatus("done");
+                break;
+            }
+        }
+        Collections.sort(this.calculationList);
+        calculationsLiveDataMutable.setValue(new ArrayList<>(this.calculationList));
+        updateSpContent(calc);
+    }
+
+    private void updateSpContent(Calculation calc) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(calc.getCalcId(), gson.toJson(calc));
+        editor.apply();
+    }
+
+
+}
